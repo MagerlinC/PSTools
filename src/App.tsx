8cc5de37ed43a8dfce089268bc435ef6b69379d2
mikkel agerlin <mikkel_ac@hotmail.com>
2021-08-19 19:24:02 +0200

    Initial scaffolding, router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "./App.scss";
+import Dropdown from "./components/dropdown/dropdown";
+import FoldableMenu, {
+  menuLocation,
+} from "./components/foldable-menu/foldable-menu";
+import LandingPage from "./components/landing-page/landing";
+import ToolController from "./components/tool-controller/tool-controller";
+import { TOOL_LIST } from "./util";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header className="App-header">
+          <Link to="/">PS Tools</Link>
+        </header>
+        <FoldableMenu
+          location={menuLocation.RIGHT}
+          routeBase={"tools"}
+          menuItems={TOOL_LIST.map((tool) => {
+            return { name: tool.name, id: tool.id };
+          })}
+        />
+        <div className="page-body">
+          <Switch>
+            <Route path="/tools/:id">
+              <ToolController />
+            </Route>
+            <Route path="/">
+              <LandingPage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
